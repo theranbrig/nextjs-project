ddab5d5c9470cf7afc7ff499154ac6e8f68cda8e
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-12-21 12:45:27 +0900

    first commit

---
+++ components/styles/MainStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const MainPage = styled.div`
+	display: flex;
+	margin: 0 auto;
+	padding: 0;
+	background-color: #333;
+	color: #fff;
+	height: 100vh;
+	font-family: 'Helvetica', sans-serif;
+	justify-content: center;
+	h1 {
+		padding-top: 100px;
+		text-align: center;
+		font-size: 50px;
+	}
+`;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { MainPage } from '../components/styles/MainStyles';
+
+export default () => (
+	<MainPage>
+		<h1>Hello World!</h1>
+	</MainPage>
+);
